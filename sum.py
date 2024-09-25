9882ce76a6d032bfeeadc4777794b69f91d1692a
ushers956 <ushershaninz@gmail.com>
2024-09-25 17:55:20 +1200

    sum

---
+++ sum.py
@@ -0,0 +1,14 @@
+# sorting values
+tree1 = 24
+tree2 = 22
+tree3 = 95
+tree4 = 32
+tree5 = 36
+ 
+# finding the sum of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 trees is: ", sum)
+
+# finding the average of trees
+average = sum/5
+print("the average of all the trees is:", average)
